c49f9c4409c22e954b203dd7164200c438da71ca
MuhammetErenMolak <muhammeterenmolak@gmail.com>
2024-12-05 11:45:15 +0300

    Add files via upload

---
+++ Muhammet Eren Molak 1083 12b/Muhammet Eren Molak 1083 12b/amp12b_1083_stp/amp12b_1083_stp/Models/Personel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amp12b_1083_stp.Models
+{
+    public class Personel
+    {
+    }
+}
